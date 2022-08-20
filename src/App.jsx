4e52c1e6fec191fe76d0d283090e033b9cc1a05d
Jose <josesantillan10@gmail.com>
2022-08-20 14:18:35 -0500

    First clear

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Main from './Components/Instance/Main'
+
+function App() {
+  return (
+    <div className="App">
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Instance/Main.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Main extends Component{
+	render(){
+		return(
+			<div>
+				<h3>Instance Methods, Parent to Child comunication</h3>
+			</div>
+		)
+	}
+}
+
+export default Main
